6dee6d9f35396fb4a41f364fe7f6c6c3f26df9dc
Roseli <ro.hleon@gmail.com>
2018-03-07 13:24:12 -0600

    Archivo HelloWorld añadido por primera vez.

---
+++ src/com/helloworld/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Mostrar "Hola Mundo";
+		System.out.println("Hola Mundo");
+
+	}
+
+}
